c8af6b3e82e562aa8b8d51868c73dbc724035707
lou <louiscuny@gmail.com>
2013-04-22 21:54:07 +0200

    first pass on demo page

--- js/application.js
+++ js/application.js
@@ -2,7 +2,7 @@ $(function(){
   $('.listy').listy({
     select: function(elem){
       elem.hide();
-    },
-    elem: 'li:visible'
+      $('#console').prepend(elem.text()+' selected !<br />');
+    }
   });
 });
